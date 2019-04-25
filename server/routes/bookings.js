1917bd4f4c2b76f7363f777aa72a8b8872c81e35
RoHi <robhizon26@yahoo.com>
2019-04-24 22:53:07 -0400

    Adds manage rental and bookings endpoint and add  delete rental endpoint

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -4,5 +4,5 @@ const UserCtrl = require("../controllers/user");
 const BookingCtrl = require("../controllers/booking");
 
 router.post("", UserCtrl.authMiddleware, BookingCtrl.createBooking);
-
+router.get("/manage", UserCtrl.authMiddleware, BookingCtrl.getUserBookings);
 module.exports = router;
